4d4c93b04e2ca480f80ffc9358ecac70a74ab4d3
Rahul P <Rahul.P@viacomcontractor.com>
2021-11-27 12:31:34 +0530

    ADD: initial commit

---
+++ eks.tf
@@ -0,0 +1,27 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
+
+  cluster_version = "1.21"
+  cluster_name    = "rahulp-test-cluster"
+
+  vpc_id          = data.aws_vpc.vpc.id
+  subnets         = data.aws_subnet_ids.private.ids
+
+  worker_groups = [
+    {
+      instance_type = "t2.large"
+      asg_max_size  = 5
+    }
+  ]
+
+  tags = {
+    created_by   = "rahulp"
+    purpose      = "test-cluster"
+    businessunit = "DevOps"
+  }
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
